687264b2dee7c38dacd402c190a881919898e115
RoRo <9920900+astro-roro@users.noreply.github.com>
2025-01-13 20:18:15 +1100

    Create pba-auto-12v.bat
    Change adjustable power output to 12v

---
+++ pba-auto-12v.bat
@@ -0,0 +1,5 @@
+# This changes the voltage on the adjustable power out within the advance sequencer - no popup required. Very good for changing brightness of flat panels automatically for different filters.
+
+for /f "delims=" %%i in ('powershell -Command "Get-Content 'C:\Users\Astro\Desktop\response.json' | ConvertFrom-Json | Select-Object -ExpandProperty data | Select-Object -First 1 | Select-Object -ExpandProperty uniqueKey"') do set uniqueKey=%%i
+curl -X POST "http://localhost:32000/Driver/PPBAdvance/Power/Variable/12?DriverUniqueKey=%uniqueKey%"
+TIMEOUT /T 1
